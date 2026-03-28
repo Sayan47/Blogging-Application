ad7fb9b55dba4675377f8d04e1c5af52098084db
Sayan Sarkar <sayrit89ss@gmail.com>
2026-03-28 23:28:39 +0530

    Changes to secret

--- src/main/java/com/sayan/blog/app/security/JWTHelper.java
+++ src/main/java/com/sayan/blog/app/security/JWTHelper.java
@@ -22,7 +22,7 @@ public class JWTHelper {
 	private final static long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 	private final static String TEST_STRING = "DEMO STRING";
 	private final static String TEST_STRING_NEW = "NEW DEMO STRING";
-	private String secret = "adafssdfasasfasfsadfsdjfhsafdhsafdthsafthfdgsafdashdfsahfdjsafdhjasfhdfasjfdhjasfdjahsfdsahjfdjsafdjashfdhjasfdjyasfdjysadgjyadadhgaffhgdfafdhafdfahfdhafhgfdghafdhafdjfafdgafdghsfgffdshgfdahafdhaffdsfhsfdhafhfafhfdaghffdahfgfsdfhsffahfafdhfsdhafdgh";
+	private String secret = "Testadafssdfasasfasfsadfsdjfhsafdhsafdthsafthfdgsafdashdfsahfdjsafdhjasfhdfasjfdhjasfdjahsfdsahjfdjsafdjashfdhjasfdjyasfdjysadgjyadadhgaffhgdfafdhafdfahfdhafhgfdghafdhafdjfafdgafdghsfgffdshgfdahafdhaffdsfhsfdhafhfafhfdaghffdahfgfsdfhsffahfafdhfsdhafdgh";
 	private SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
 
 	public String getUserNameFromToken(String token) {
@@ -51,9 +51,10 @@ public class JWTHelper {
 
 	private String doGenerateToken(Map<String, Object> claims, String subject) {
 		return Jwts.builder().claims(claims).subject(subject).issuedAt(new Date(System.currentTimeMillis())
-				
-				)
-				.expiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000)).signWith(secretKey).compact();
+
+		)
+				.expiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000)).signWith(secretKey)
+				.compact();
 	}
 
 	public Boolean validateToken(String token, UserDetails userDetails) {
