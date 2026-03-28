a7c2eb4881c47a72832c4c37ca5448753f8dd8c3
Sayan Sarkar <37772790+Sayan47@users.noreply.github.com>
2026-03-29 00:06:02 +0530

    Update JWTHelper.java

--- src/main/java/com/sayan/blog/app/security/JWTHelper.java
+++ src/main/java/com/sayan/blog/app/security/JWTHelper.java
@@ -20,8 +20,8 @@ public class JWTHelper {
 
 	// Token validity in seconds
 	private final static long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
-	private final static String TEST_STRING = "DEMO STRING3";
-	private final static String TEST_STRING_NEW = "NEW DEMO STRING3";
+	private final static String TEST_STRING = "DEMO STRING33";
+	private final static String TEST_STRING_NEW = "NEW DEMO STRING33";
 	private String secret = "sdadafssdfasasfasfsadfsdjfhsafdhsafdthsafthfdgsafdashdfsahfdjsafdhjasfhdfasjfdhjasfdjahsfdsahjfdjsafdjashfdhjasfdjyasfdjysadgjyadadhgaffhgdfafdhafdfahfdhafhgfdghafdhafdjfafdgafdghsfgffdshgfdahafdhaffdsfhsfdhafhfafhfdaghffdahfgfsdfhsffahfafdhfsdhafdgh";
 	private SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
 
