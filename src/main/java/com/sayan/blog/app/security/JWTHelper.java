7200b0eca0b54750bf0a8faf8c52f12a0de9bb28
Sayan Sarkar <sayrit89ss@gmail.com>
2026-03-28 23:00:26 +0530

    Adding comments

--- src/main/java/com/sayan/blog/app/security/JWTHelper.java
+++ src/main/java/com/sayan/blog/app/security/JWTHelper.java
@@ -18,6 +18,7 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JWTHelper {
 
+	// Token validity in seconds
 	private final static long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 	private String secret = "adafssdfasasfasfsadfsdjfhsafdhsafdthsafthfdgsafdashdfsahfdjsafdhjasfhdfasjfdhjasfdjahsfdsahjfdjsafdjashfdhjasfdjyasfdjysadgjyadadhgaffhgdfafdhafdfahfdhafhgfdghafdhafdjfafdgafdghsfgffdshgfdahafdhaffdsfhsfdhafhfafhfdaghffdahfgfsdfhsffahfafdhfsdhafdgh";
 	private SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
@@ -47,7 +48,9 @@ public class JWTHelper {
 	}
 
 	private String doGenerateToken(Map<String, Object> claims, String subject) {
-		return Jwts.builder().claims(claims).subject(subject).issuedAt(new Date(System.currentTimeMillis()))
+		return Jwts.builder().claims(claims).subject(subject).issuedAt(new Date(System.currentTimeMillis())
+				
+				)
 				.expiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000)).signWith(secretKey).compact();
 	}
 
